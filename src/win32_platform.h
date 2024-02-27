2e124b97acacb856014123debcf79d5f0b0496a5
xfangfang <2553041586@qq.com>
2024-02-27 17:21:44 +0800

    Win32: Add dark mode for title bar https://github.com/glfw/glfw/pull/2228

--- src/win32_platform.h
+++ src/win32_platform.h
@@ -341,10 +341,12 @@ typedef HRESULT (WINAPI * PFN_DwmIsCompositionEnabled)(BOOL*);
 typedef HRESULT (WINAPI * PFN_DwmFlush)(VOID);
 typedef HRESULT(WINAPI * PFN_DwmEnableBlurBehindWindow)(HWND,const DWM_BLURBEHIND*);
 typedef HRESULT (WINAPI * PFN_DwmGetColorizationColor)(DWORD*,BOOL*);
+typedef HRESULT (WINAPI * PFN_DwmSetWindowAttribute)(HWND,DWORD,LPCVOID,DWORD);
 #define DwmIsCompositionEnabled _glfw.win32.dwmapi.IsCompositionEnabled
 #define DwmFlush _glfw.win32.dwmapi.Flush
 #define DwmEnableBlurBehindWindow _glfw.win32.dwmapi.EnableBlurBehindWindow
 #define DwmGetColorizationColor _glfw.win32.dwmapi.GetColorizationColor
+#define DwmSetWindowAttribute _glfw.win32.dwmapi.SetWindowAttribute
 
 // shcore.dll function pointer typedefs
 typedef HRESULT (WINAPI * PFN_SetProcessDpiAwareness)(PROCESS_DPI_AWARENESS);
@@ -430,6 +432,10 @@ typedef VkBool32 (APIENTRY *PFN_vkGetPhysicalDeviceWin32PresentationSupportKHR)(
 #define GLFW_WGL_CONTEXT_STATE          _GLFWcontextWGL wgl;
 #define GLFW_WGL_LIBRARY_CONTEXT_STATE  _GLFWlibraryWGL wgl;
 
+typedef BOOL (WINAPI * ShouldAppsUseDarkModePtr)();
+typedef DWORD (WINAPI * GetImmersiveColorFromColorSetExPtr)(UINT,UINT,BOOL,UINT);
+typedef int (WINAPI * GetImmersiveColorTypeFromNamePtr)(const WCHAR*);
+typedef int (WINAPI * GetImmersiveUserColorSetPreferencePtr)(BOOL,BOOL);
 
 // WGL-specific per-context data
 //
@@ -562,6 +568,7 @@ typedef struct _GLFWlibraryWin32
         PFN_DwmFlush                    Flush;
         PFN_DwmEnableBlurBehindWindow   EnableBlurBehindWindow;
         PFN_DwmGetColorizationColor     GetColorizationColor;
+        PFN_DwmSetWindowAttribute       SetWindowAttribute;
     } dwmapi;
 
     struct {
@@ -589,6 +596,16 @@ typedef struct _GLFWlibraryWin32
         PFN_ImmSetCandidateWindow       ImmSetCandidateWindow_;
         PFN_ImmSetOpenStatus            ImmSetOpenStatus_;
     } imm32;
+
+    struct {
+        HINSTANCE                              instance;
+        GLFWbool                               uxThemeAvailable;
+        GLFWbool                               darkTitleAvailable;
+        ShouldAppsUseDarkModePtr               ShouldAppsUseDarkMode;
+        GetImmersiveColorFromColorSetExPtr     GetImmersiveColorFromColorSetEx;
+        GetImmersiveColorTypeFromNamePtr       GetImmersiveColorTypeFromName;
+        GetImmersiveUserColorSetPreferencePtr  GetImmersiveUserColorSetPreference;
+    } uxtheme;
 } _GLFWlibraryWin32;
 
 // Win32-specific per-monitor data
