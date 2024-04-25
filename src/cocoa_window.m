d323351a77c3b22adfd97b36b09793295584ca8b
xfangfang <2553041586@qq.com>
2024-04-25 22:33:23 +0800

    macOS: Display colors correctly on a wide color gamut screen

--- src/cocoa_window.m
+++ src/cocoa_window.m
@@ -1013,6 +1013,8 @@ static GLFWbool createNativeWindow(_GLFWwindow* window,
             [window->ns.object zoom:nil];
     }
 
+    [window->ns.object setColorSpace:[NSColorSpace sRGBColorSpace]];
+
     if (strlen(wndconfig->ns.frameName))
         [window->ns.object setFrameAutosaveName:@(wndconfig->ns.frameName)];
 
